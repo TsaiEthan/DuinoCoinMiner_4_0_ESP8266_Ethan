925a4e431e0d92fa9e381e6c38dd9ae27525b2a8
Ethan Tsai <919ethan@gmail.com>
2024-01-06 11:25:54 +0800

    基礎程式碼、調整好的WiFiManager設定

--- bulid/core/cbuf.cpp.d
+++
@@ -1,6 +0,0 @@
-C:\Users\yamah\Documents\Arduino\DuinoCoinMiner_4_0_ESP8266_Ethan\bulid\core\cbuf.cpp.o: \
- C:\Users\yamah\AppData\Local\Arduino15\packages\esp8266\hardware\esp8266\3.1.1\cores\esp8266\cbuf.cpp \
- C:\Users\yamah\AppData\Local\Arduino15\packages\esp8266\hardware\esp8266\3.1.1\cores\esp8266\CommonHFile.h \
- C:\Users\yamah\Documents\Arduino\DuinoCoinMiner_4_0_ESP8266_Ethan\bulid\core\DuinoCoinMiner_4_0_ESP8266_Ethan.ino.globals.h \
- C:\Users\yamah\AppData\Local\Arduino15\packages\esp8266\hardware\esp8266\3.1.1\cores\esp8266\cbuf.h \
- C:\Users\yamah\AppData\Local\Arduino15\packages\esp8266\hardware\esp8266\3.1.1/tools/sdk/include/c_types.h
--- bulid/core/gdb_hooks.cpp.d
+++
@@ -1,8 +0,0 @@
-C:\Users\yamah\Documents\Arduino\DuinoCoinMiner_4_0_ESP8266_Ethan\bulid\core\gdb_hooks.cpp.o: \
- C:\Users\yamah\AppData\Local\Arduino15\packages\esp8266\hardware\esp8266\3.1.1\cores\esp8266\gdb_hooks.cpp \
- C:\Users\yamah\AppData\Local\Arduino15\packages\esp8266\hardware\esp8266\3.1.1\cores\esp8266\CommonHFile.h \
- C:\Users\yamah\Documents\Arduino\DuinoCoinMiner_4_0_ESP8266_Ethan\bulid\core\DuinoCoinMiner_4_0_ESP8266_Ethan.ino.globals.h \
- C:\Users\yamah\AppData\Local\Arduino15\packages\esp8266\hardware\esp8266\3.1.1/tools/sdk/include/ets_sys.h \
- C:\Users\yamah\AppData\Local\Arduino15\packages\esp8266\hardware\esp8266\3.1.1/tools/sdk/include/c_types.h \
- C:\Users\yamah\AppData\Local\Arduino15\packages\esp8266\hardware\esp8266\3.1.1/tools/sdk/include/eagle_soc.h \
- C:\Users\yamah\AppData\Local\Arduino15\packages\esp8266\hardware\esp8266\3.1.1\cores\esp8266\gdb_hooks.h
